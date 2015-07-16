42149a223530c0ebcfa1391b2c968973270bb23c
codingbubble <codingbubble@foxmail.com>
2015-07-16 12:50:00 +0800

    可进行四则运算，支持百分号%和多层括号

---
+++ src/bubble/calculator/MainActivity.java
@@ -0,0 +1,407 @@
+package bubble.calculator;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import bubble.calculator.utils.ParseExpression;
+
+import com.bubble.calculator.R;
+
+/**
+ * <p>Title: MainActivity</p>
+ * <p>Description: Android计算器</p>
+ * @version 1.0   
+ * @since JDK 1.8.0_45
+ * @author bubble
+ * @date 2015-7-2
+ */
+public class MainActivity extends Activity implements OnTouchListener,OnClickListener{
+	private ViewPager mPager;	//页卡内容
+	private List<View> listViews;	//页面列表
+	
+	final static String ERROR = "格式错误";
+	String parenthesis = "( )";
+	String[] btTexts = new String[]{
+			"C", "÷", "×", "D",
+			"7", "8", "9", "-",
+			"4", "5", "6", "+",
+			"1", "2", "3", parenthesis,
+			"0", ".", "%", "="
+	};
+	
+	GridLayout gridLayout;
+	EditText showEditText;
+	
+	String resultString;
+	String exp;
+	String expAndResult;
+	boolean cursorEnd = true;	//光标是否在尾端，默认为真
+	String frontExp = exp;	//光标前的表达式,默认光标在尾端
+	String rearExp = "";	//光标后的表达式,默认光标在尾端
+	String regOperator = "\\+|-|×|÷";
+	char lastChar = ' ';
+
+	StringBuilder history = new StringBuilder();	//保存历史记录
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.main_activity_layout);
+        
+        showEditText = (EditText)findViewById(R.id.showET);
+         
+        initVal();	//初始化字符串变量
+        initUI();	//初始化界面
+    }
+    
+    /**
+     * <p>Title: initCal</p>
+     * <p>Description: 初始化</p>
+     * @author bubble
+     * @date 2015-7-3
+     */
+    private void initVal(){
+    	resultString = "";
+    	exp = "";
+    	expAndResult = "";
+    	frontExp = exp;
+    	rearExp = "";
+    	cursorEnd = true;
+    	showEditText.setText("");
+    	showEditText.setSelection(exp.length());
+    }
+ 
+    private void initUI(){
+    	Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        
+        int cellWidth = (int)( (size.x - 3) / 4);
+        int cellHeight = (int)( (size.y - 5) / 7 );
+
+		showEditText.setTextIsSelectable(true);
+		showEditText.setBackgroundColor(Color.WHITE);
+		GridLayout.LayoutParams tvParams = (GridLayout.LayoutParams)showEditText.getLayoutParams();
+		tvParams.height = (int)( size.y - cellHeight * 5 - 44 );
+		showEditText.setLayoutParams(tvParams);
+		
+        gridLayout = (GridLayout)findViewById(R.id.main_activity);
+        GridLayout.Spec rowSpec;
+        GridLayout.Spec columnSpec;
+        gridLayout.setBackgroundColor(Color.parseColor("#D1D1D1"));
+        GridLayout.LayoutParams cellParams;
+        
+        //批量初始化按钮
+        Button btn[] = new Button[btTexts.length];
+
+        for(int i = 0; i < btTexts.length; i++){
+        	btn[i] = new Button(this);
+        	btn[i].setText(btTexts[i]);
+        	btn[i].setTextColor(Color.parseColor("#666666"));
+        	btn[i].setTextSize(30);
+        	btn[i].setBackgroundColor(Color.WHITE);
+        	
+        	rowSpec = GridLayout.spec(i/4 + 2);  
+        	columnSpec = GridLayout.spec(i % 4 );  
+        	cellParams = new GridLayout.LayoutParams(rowSpec, columnSpec); 
+            cellParams.width = cellWidth;  
+            cellParams.height = cellHeight; 
+            cellParams.setMargins(0, 1, 1, 0);
+            gridLayout.addView(btn[i], cellParams);  
+
+            btn[i].setOnTouchListener(this);
+            
+            btn[i].setOnClickListener(this);
+        }
+    }
+	/* (non-Javadoc)
+     * <p>Title: onTouch</p>
+     * <p>Description: </p>
+     * @see android.view.View.OnTouchListener#onTouch(android.view.View, android.view.MotionEvent)
+     * @param arg0
+     * @param event
+     * @return
+     * @author bubble
+     * @date 2015-7-13 下午6:45:19
+     */
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+    	Button bt = (Button)v;
+    	switch ( event.getAction() ){
+    	case MotionEvent.ACTION_DOWN:
+    		bt.setBackgroundColor(Color.parseColor("#F2F2F2"));
+    		break;
+    	case MotionEvent.ACTION_UP:
+    		bt.setBackgroundColor(Color.WHITE);
+    		bt.performClick();
+    		break;
+    	default:
+    		break;
+    	}
+    	return true;
+    }
+    
+    /* (non-Javadoc)
+     * <p>Title: onClick</p>
+     * <p>Description: </p>
+     * @see android.view.View.OnClickListener#onClick(android.view.View)
+     * @param v
+     * @author bubble
+     * @date 2015-7-13 下午6:45:32
+     */
+    @Override
+	public void onClick(View v) {
+		Button bt = (Button)v;
+		String btText = bt.getText().toString();
+		
+		exp = showEditText.getText().toString();
+        expAndResult = "";
+        frontExp = exp;
+        rearExp = "";
+		String inputString = bt.getText().toString();
+		
+		if ( resultString.matches(ERROR) ){
+			initVal();
+		}
+		
+		int cursorIndex = showEditText.getSelectionStart();
+		if ( cursorIndex != showEditText.getText().length()) {
+			cursorEnd = false;
+		}
+		if ( ! cursorEnd ){
+			if ( resultString == ""){
+				if ( (exp.length() > 0) ){
+					if ( cursorIndex > 0 ){
+						frontExp = exp.substring(0, cursorIndex);
+						rearExp = exp.substring(cursorIndex, exp.length());
+					}
+					else if ( cursorIndex == 0 ){
+						frontExp = "";
+						rearExp = exp;
+					}
+				}
+			}
+			else{
+				initVal();
+			}
+		}
+		
+		
+		if(btText.matches("[0-9]|\\+|-|×|÷|\\.|(\\( \\))|=")){
+			//已经运算过一次并有了运算结果
+			if((resultString != "")){
+				//判断光标位置
+				if ( cursorEnd ){	//光标在尾端
+					if(ParseExpression.isOperator(inputString)){	//如果输入的是运算符，表明是继续运算
+						exp = resultString;
+						resultString = "";
+					}
+					else if(inputString.matches("=") ){	//若输入"="，继续重复上一次的运算符运算
+						exp = exp.replaceAll("\n=.*", "");
+						String[] op = ParseExpression.splitInfixExp(exp);
+						int opLen = op.length;
+						exp = resultString + op[opLen - 2] + op[opLen - 1];
+						resultString = "";
+					}
+					else if(inputString == parenthesis){	//若输入括号
+						exp = "(" + resultString;
+						inputString = "";
+						resultString = "";
+					}
+					else {	//重新开始新的运算
+						initVal();
+					}
+				}
+				else{	//光标不在尾端，则开始新的运算
+					initVal();
+				}
+			}
+			
+			if ( inputString.matches("\\.") ) {
+				if ( cursorEnd ) {
+					if ( ParseExpression.appendDotValid(exp) ) {
+						if ( exp.matches(".*?(" + regOperator + "|\\()$|()") )
+							inputString = "0.";
+					}
+					else
+						return;
+				}
+				else {
+					if ( ParseExpression.appendDotValid(frontExp) ){
+						if ( frontExp.matches(".*?(" + regOperator + "|\\()$|()") ){
+							exp = frontExp + "0." + rearExp;
+							inputString = "";
+							showEditText.setSelection(cursorIndex + 2);
+						}
+						else {
+							exp = frontExp + "." + rearExp;
+							inputString = "";
+							showEditText.setSelection(cursorIndex + 1);
+						}
+					}
+					else{
+						return;
+					}
+				}
+			}
+			else if( cursorEnd ){
+				if ( inputString == parenthesis ) {
+					inputString = ParseExpression.inputParenthesis(exp);
+					if ( inputString.matches("\\)") ) {
+						if(exp.length() > 1)
+							lastChar = exp.charAt(exp.length()-1);
+						if (lastChar == '.')
+							exp = exp.substring(0, exp.length()-1);
+					}
+				}
+				else if ( ParseExpression.isOperator(inputString) ) {
+					if( exp.endsWith("(")){
+						if( ! inputString.matches("-") )
+							return;
+					}
+					//如果lastCHar是运算符且倒数第二个字符是数字，则替换成input输入的运算符
+					else if(exp.length() > 1){
+						lastChar = exp.charAt(exp.length()-1);
+						String penultCharString = String.valueOf(exp.charAt(exp.length()-2));
+						if ( ParseExpression.isOperator(lastChar) ){
+							if(penultCharString.matches("[0-9]|\\)|\\.|%"))
+								exp = exp.substring(0, exp.length()-1);
+							else{
+								return;
+							}
+						}
+						else if ( (lastChar == '.')){
+							exp = exp.substring(0, exp.length()-1);
+						}
+					}
+					else if ( exp.length() == 1 ){
+						if( exp.matches("-") )
+							return;
+					}
+					else if( ! inputString.matches("-") )
+						return;
+				}
+				else if (inputString.matches("[0-9]")){
+					if( (exp.length() > 0) ){
+						lastChar = exp.charAt(exp.length()-1);
+						if( (lastChar == ')') || (lastChar == '%') )
+							return;
+					}
+					if(exp.endsWith("0")){
+						if(exp.length() == 1)
+							exp = "";
+						else
+							exp= exp.replaceAll("(.*?)(" + regOperator + "|\\()(0)$","$1$2");
+					}
+				}
+				lastChar = ' ';
+			}
+			
+			if (inputString.equals("=")) {
+				//若表达式以运算符结尾，则去掉末端的运算符 再执行运算
+				exp = exp.replaceAll("(.*?)(\\+|-|×|÷)(\\(?)$", "$1");
+				if( !ParseExpression.isParenthesisMatch(exp))
+					return;
+				if( exp == "")
+					return;
+				
+				resultString = ParseExpression.calInfix(exp);
+				expAndResult = exp + "\n=" + resultString;
+				history.append(expAndResult + "\n");
+				showEditText.setText(expAndResult);
+			}
+			else {
+				if ( cursorEnd ){
+					exp = exp + inputString;
+				}
+				else{
+					if( inputString == parenthesis)
+						inputString = "()";
+					exp = frontExp + inputString + rearExp;
+				}
+				showEditText.setText("");
+				showEditText.setText(exp);
+			}
+		}
+		
+		else if(btText.matches("%")){
+			if ( cursorEnd ){
+				if((resultString != "")){
+					exp = resultString;
+					resultString = "";
+				}
+				else if( !exp.matches(".*?(\\)|[0-9])$") ){
+	    			return;
+				}
+				showEditText.setText(exp + "%");
+			}
+			else{
+				if((resultString != ""))
+					return;
+				showEditText.setText(frontExp + "%" + rearExp);
+			}
+    	}
+		
+    	else if(btText.matches("C")){
+			initVal();
+			return;
+    	}
+		
+    	else if(btText.matches("D")){
+	        
+			if (frontExp.equals(""))
+				return;
+			
+			if(resultString != ""){
+				initVal();
+				return;
+			}
+			
+			frontExp = frontExp.substring(0, frontExp.length()-1);
+			showEditText.setText(frontExp + rearExp);
+			showEditText.setSelection(cursorIndex - 1);
+			return;
+    	}
+		
+		if ( cursorEnd || (inputString == "=") )
+			showEditText.setSelection(showEditText.getText().length());
+		else if ( cursorIndex < showEditText.getText().length() )
+			showEditText.setSelection(cursorIndex + 1);
+		else if ( !cursorEnd && inputString == "()")
+			showEditText.setSelection(cursorIndex + 2);
+	}
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+      
+}
---
+++ src/bubble/calculator/utils/ParseExpression.java
@@ -0,0 +1,401 @@
+package bubble.calculator.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * <p>Title: ParseInfixExp</p>
+ * <p>Description: 计算中缀表达式的值</p>
+ * <p>Company: </p> 
+ * @version 1.0   
+ * @since JDK 1.8.0_45
+ * @author bubble
+ * @date 2015-7-2
+ */
+public class ParseExpression {
+	final static String ERROR = "格式错误";
+	final static String[] ERROR_ARRAY = ERROR.split("");
+	static String regOperator = "\\+|-|×|÷";
+	
+	private static final Map<String, Integer> OPERATORS = new HashMap<String, Integer>();
+ 
+	static {	//运算符优先级
+		OPERATORS.put( "+", 0);
+		OPERATORS.put( "-", 0);
+		OPERATORS.put( "×", 1);
+		OPERATORS.put( "÷", 1);
+	 }
+
+	/**
+	 * <p>Title: isOperator</p>
+	 * <p>Description: 判断字符串是否是运算符</p>
+	 * @param str 
+	 * @return boolean
+	 * @date 2015-7-2 
+	 */
+	public static boolean isOperator(String str) {
+		return OPERATORS.containsKey(str);
+	}
+	
+	/**
+	 * <p>Title: isOperator</p>
+	 * <p>Description: 判断字符是否是运算符</p>
+	 * @param ch
+	 * @return boolean
+	 * @author bubble
+	 * @date 2015-7-13 下午6:02:58
+	 */
+	public static boolean isOperator(char ch) {
+		String s = String.valueOf(ch);
+		return OPERATORS.containsKey(s);
+	}
+	
+ 
+	/**
+	 * <p>Title: comparePrior</p>
+	 * <p>Description: 比较运算符优先级</p>
+	 * @param op1 运算符
+	 * @param op2 运算符
+	 * @return op1<op2,返回负数，op1=op2返回0，op1>op2返回正数
+	 * @date 2015-7-2
+	 */
+	public static final int comparePrior(String op1, String op2) {
+		if (! isOperator(op1) || !isOperator(op2)) {
+			throw new IllegalArgumentException( "Invalid operators:" + op1 + " " + op2);
+		}
+       
+        return OPERATORS.get(op1) - OPERATORS.get(op2);
+ }
+ 
+	/**
+	 * <p>Title: splitInfixExp</p>
+	 * <p>Description: 分离中缀表达式，将操作数、运算符分离并存进数组里</p>
+	 * @param infixExp 中缀表达式
+	 * @return 返回存放操作数、运算符的String数组
+	 * @date 2015-7-2
+	 */
+	public static String[] splitInfixExp(String infixExp) {
+		if ( infixExp.matches(".*?\\(|" + regOperator + "$")){
+	    	   return ERROR_ARRAY;
+	       }
+		else if ( ! isParenthesisMatch(infixExp) ){	//括号不匹配
+			return ERROR_ARRAY;
+		}
+		else if ( infixExp.matches(".*?" + regOperator + "(" +regOperator + ")+.*?" ) ){	//出现2个相邻运算符
+			return ERROR_ARRAY;
+		}
+		else if ( infixExp.matches(".*?(\\.|%)[0-9](\\.|%).*?") ){
+			return ERROR_ARRAY;
+		}
+		
+		ArrayList<String> strArr = new ArrayList<String>();
+       
+		StringBuilder lastOperand = new StringBuilder();
+		//遍历中缀字符数组
+		for ( char ch: infixExp.toCharArray()) {
+			String str = Character.toString(ch);
+			String arrString = strArr.toString().replaceAll("(.*?)(\\])$", "$1");
+			
+			if (str.matches("\\(")){
+				strArr.add(str);
+				lastOperand.setLength(0);
+			}
+			else if ( str.matches("\\)") ){
+				if(lastOperand.length() != 0){
+					strArr.add(lastOperand.toString());
+					lastOperand.setLength(0);
+					strArr.add(str);
+				}
+				else if(arrString.endsWith(")")){
+					strArr.add(str);
+				}
+			}
+			else if ( str.matches("%") ){
+				if( arrString.endsWith(")") ){
+					strArr.add(str);
+				}
+				else if( lastOperand.length() > 0){
+					strArr.add(lastOperand.toString());
+					lastOperand.setLength(0);
+					strArr.add(str);
+				}
+			}
+			else if ( isOperator(str) ) {
+				if(lastOperand.length() != 0){
+					strArr.add(lastOperand.toString());
+				}
+				else if( (arrString.length() == 1) || (arrString.endsWith("(")) ){
+					if (str.equals("-")){	//负号
+						lastOperand.append(ch);
+						continue;
+					}
+				}
+				
+				strArr.add(str);
+				lastOperand.setLength(0);
+			}
+			else if ( str.matches("[0-9]|\\.") ){
+				lastOperand.append(str);
+			}
+			else {
+				return ERROR_ARRAY;
+			}
+		}
+   
+		if(lastOperand.length() != 0){
+			strArr.add(lastOperand.toString());
+		}
+       
+		String[] outputs = new String[strArr.size()];
+        return strArr.toArray(outputs);
+	}   
+	
+	/**
+	 * <p>Title: infix2Suffix</p>
+	 * <p>Description: 将中缀表达式转换成后缀表达式</p>
+	 * @param infixExp 中缀表达式
+	 * @return 后缀表达式
+	 * @date 2015-7-2
+	 */
+	public static String infix2Suffix(String infixExp) {
+		String suffixExp = "";
+	   
+		String[] inputs = splitInfixExp(infixExp);
+	   
+		if (inputs == null)
+			return null;
+	   
+		Stack<String> stack = new Stack<String>();
+	   
+		for (String input: inputs) {
+			if ( input.matches("\\(") ){
+				stack.push(input);
+			}
+			else if (input.matches("%")){
+				stack.push(input);
+			}
+			else if( input.matches("\\)") ){
+				while ( !stack.empty() ){
+					while( !stack.peek().matches("\\(") )
+						suffixExp =suffixExp + " " + stack.pop();
+					if(stack.peek().matches("\\("))
+						break;
+				}
+				if( stack.peek().matches("\\(") ){
+					stack.pop();
+				}
+				else
+					return ERROR;
+			}
+			else if ( isOperator(input) ) {
+				while ( !stack.empty() ) {
+					if( isOperator(stack.peek()) ){
+						if ( comparePrior(input, stack.peek()) <= 0) {
+							suffixExp =suffixExp + " " + stack.pop();
+							continue;
+						}
+					}
+					else if( stack.peek().matches("%") ){
+						suffixExp =suffixExp + " " + stack.pop();
+						continue;
+					}
+					break;
+				}
+				stack.push(input);
+			}
+			else {
+				suffixExp = suffixExp + " " + input;
+			}
+		}
+	   
+		while (!stack.empty()) {
+			suffixExp = suffixExp + " " + stack.pop();
+		}
+	       
+		return suffixExp.trim();
+	} 
+	
+	/**
+	 * <p>Title: calSuffix</p>
+	 * <p>Description: 计算后缀表达式的值</p>
+	 * @param suffixExp 后缀表达式
+	 * @return 表达式的值
+	 * @date 2015-7-2
+	 */
+	public static String calSuffix(String suffixExp) {
+		String[] expression = suffixExp.split(" ");
+		String resultString = "";
+		String v1 = "";
+		String v2 = "";
+		String val = "";
+		//字符串是否为合法操作数
+		String regNum = "[0-9]+(\\.[0-9]+)?";
+		
+		Stack<String> stack = new Stack<String>();
+		for (String op: expression) {
+			if( isOperator(op)) {
+				if (stack.size() < 2)
+					return ERROR;
+		 
+				v2 = stack.pop();
+				v1 = stack.pop();
+	 
+				if ( v1.matches(regNum) && v2.matches(regNum) ){
+					switch(op.charAt(0)){
+						case '+':
+							val = Arith.add(v1, v2);
+							break;
+						case '-':
+							val = Arith.sub(v1, v2);
+							break;
+						case '×':
+							val = Arith.mul(v1, v2);
+							break;
+						case '÷':
+							if( v2.matches("0") )
+								return ERROR;
+							val = Arith.div(v1, v2);
+							break;
+					}
+					stack.push(val);
+				}
+				else 
+					return ERROR;
+			}
+			else if( op.matches("%") ){
+				if( stack.size() < 1)
+					return ERROR;
+				
+				v1 = stack.pop();
+				v2 = "100";
+				if ( ! v1.matches(regNum) )
+					return ERROR;
+				
+				val = Arith.div(v1, v2);
+				stack.push(val);
+			}
+			else if ( op.matches(regNum) ){
+				stack.push(op);
+			}
+			else{
+				return ERROR;
+			}
+		}
+		
+		resultString = stack.pop();
+
+		if ( ! stack.empty() ){
+	    	return ERROR;
+	    }
+		
+		//格式化数值
+		if(resultString.indexOf(".") > 0){  
+			resultString = resultString.replaceAll("(0+?)$", "");//去掉多余的0  
+			resultString = resultString.replaceAll("(\\.)$", "");//如最后一位是.则去掉  
+        }  
+		else if(resultString.indexOf(".") == 0){
+			resultString = "0" + resultString;
+		}
+		
+    	return resultString;
+	}
+	 
+ 	// Evaluate an infix expression.
+	//计算中缀表达式
+	/**
+	 * <p>Title: calInfix</p>
+	 * <p>Description: 计算中缀表达式的值</p>
+	 * @param exp 中缀表达式
+	 * @return 中缀表达式的值
+	 * @date 2015-7-2
+	 */
+	public static String calInfix(String exp) {
+		String suffixExp = infix2Suffix(exp);
+		if (suffixExp == null)
+			return "";
+		
+		return calSuffix(suffixExp);
+	}
+
+    /**
+     * <p>Title: inputParenthesis</p>
+     * <p>Description: 输入括号，"("和")"</p>
+     * @return "("或")"
+     * @author bubble
+     * @date 2015-7-6 上午11:15:17
+     */
+    public static String inputParenthesis(String expression){
+    	String inputParenthesis = "";
+    	if(expression.length() > 0){
+    		String lastCharString = String.valueOf(expression.charAt(expression.length()-1));
+    		if(isOperator(lastCharString) || lastCharString.matches("\\(") )
+    			inputParenthesis = "(";
+    		else if(lastCharString.matches( "[0-9]|\\.|\\)|%") ){
+    			boolean b = isParenthesisMatch(expression);
+    			if(b)
+    				return "";
+    			else
+    				inputParenthesis = ")";
+    		}
+    		else 
+    			return "";
+    	}
+    	else{ 
+    		inputParenthesis = "(";
+    	}
+    	return inputParenthesis;
+    }
+    
+    
+    /**
+     * <p>Title: isParenthesisMatch</p>
+     * <p>Description: 判断表达式的小括号"()"是否匹配</p>
+     * @param expression 表达式
+     * @return 匹配返回true；不匹配返回false
+     * @author bubble
+     * @date 2015-7-10 下午3:38:04
+     */
+    public static boolean isParenthesisMatch(String expression){
+    	Stack<Character> parenthesisStack = new Stack<Character>();
+		char[] expArray = expression.toCharArray();
+		for (char c:expArray){
+			if(c == '(')
+				parenthesisStack.push(c);
+			else if( (c == ')') && (! parenthesisStack.isEmpty()) )
+				parenthesisStack.pop();
+		}
+		if( parenthesisStack.isEmpty() )
+			return true;
+		else 
+			return false;
+    }
+    
+    /**
+	 * <p>Title: insertDot2EndValid</p>
+	 * <p>Description: 判断在表达式尾端追加的点'.'是否有效</p>
+	 * @param expression 运算表达式
+	 * @return 输入的点'.'有效则返回true
+	 * @date 2015-7-3
+	 */
+    public static boolean appendDotValid(String expression) {
+		//运算表达式为空，则可以输入点，有效
+		if (expression.equals("")) 
+			return true;
+		
+		int expLen = expression.length();
+		for (int i = expLen - 1; i >= 0; --i) {
+			char ch = expression.charAt(i);
+			if ( isOperator(ch) || (ch == '(') )
+				return true;
+			else if ( ch == '.' || ch == '%' || ch == ')' )
+				return false;
+		}
+		
+		return true;
+	}
+}
